0e7b26ac343b2bc1e8eece6db80759978de20f05
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-16 19:23:34 +0600

    upd:final1

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -2,15 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DivisionByZero {
     public static void main(String[] args) {
-//        float number = 10f;
-//        System.out.println(number/0);
-        int number = 4123;
-        System.out.println((number / 1000) + (number % 1000 / 100) + (number % 1000 % 10) + (number % 1000 % 100 % 10));
-//        System.out.println(number % 1000 % 10);
-        System.out.println(number / 1000);
-        System.out.println(number % 1000 / 100);
-        System.out.println(number % 1000 % 100 % 10);
-        System.out.println(number % 1000 % 100 / 10);
+        float number = 10f;
+        System.out.println(number/0);
     }
 }
 
